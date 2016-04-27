fc70b77c5bd53a472a427205a03b41f095cc2126
Aníbal Limón <anibal.limon@linux.intel.com>
2016-04-27 16:26:58 -0500

    oeqa2/test/case.py: Remove unused method oe_id use id instead.
    Signed-off-by: Aníbal Limón <anibal.limon@linux.intel.com>

--- oeqa2/test/case.py
+++ oeqa2/test/case.py
@@ -4,6 +4,3 @@ class OETestCase(unittest.TestCase):
     def __init__(self, tc, methodName='runTest', *args, **kwargs):
         super(OETestCase, self).__init__(methodName)
         self.tc = tc
-
-    def oe_id(self):
-        return "%s.%s" % (self.__module__, self.id())
