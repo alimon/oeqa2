91325d14461e7b6f090fc4b6caf928332ad501bb
Aníbal Limón <anibal.limon@linux.intel.com>
2016-05-04 14:42:29 -0500

    oeqa2/runtime/ssh: Fix OETestDepends attribute type.
    Signed-off-by: Aníbal Limón <anibal.limon@linux.intel.com>

--- oeqa2/runtime/ssh.py
+++ oeqa2/runtime/ssh.py
@@ -6,21 +6,21 @@ from oeqa2.test.case import OETestCase
 from oeqa2.test.decorator import OETestDepends, OETestID
 
 class SshTest2(OETestCase):
-    @OETestDepends('SshTest.test_ssh_20')
+    @OETestDepends(['SshTest.test_ssh_20'])
     def test_ssh_30(self):
         pass
 
 class SshTest(OETestCase):
-    @OETestDepends('test_ssh_10')
+    @OETestDepends(['test_ssh_10'])
     def test_ssh_20(self):
         pass
 
-    @OETestDepends(('test_ssh'))
+    @OETestDepends(['test_ssh'])
     def test_ssh_10(self):
         pass
 
     @OETestID(224)
-    @OETestDepends(('basic.ping.PingTest.test_ping'))
+    @OETestDepends(['basic.ping.PingTest.test_ping'])
     def test_ssh(self):
         (status, output) = self.target.run('uname -a')
         self.assertEqual(status, 0, msg="SSH Test failed: %s" % output)
